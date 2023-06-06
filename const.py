f3b8ab146a22a114d6f81afc47d42aa2af1d0787
smk762 <smk762@iinet.net.au>
2023-06-07 01:19:30 +0800

    Parse .env in const.py

--- const.py
+++ const.py
@@ -3,20 +3,27 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-KOMODO_CONF_PATH = os.getenv(f'KOMODO_CONF_PATH')
-KMD_RPCPORT = os.getenv(f'KMD_RPCPORT')
-KMD_RPCPASS = os.getenv(f'KMD_RPCPASS')
-KMD_RPCUSER = os.getenv(f'KMD_RPCUSER')
-KMD_AC_NAME = os.getenv(f'MARTY')
-USER = os.getlogin()
-
-if not KOMODO_CONF_PATH:
-    KOMODO_CONF_PATH = f'/home/{USER}/.komodo/'
-if not KMD_RPCPORT:
-    KMD_RPCPORT = 7771
-if not KMD_RPCPASS:
-    KMD_RPCPASS = 'rpcpassword'
-if not KMD_RPCUSER:
-    KMD_RPCUSER = 'rpcuser'
-if not KMD_AC_NAME:
-    KMD_AC_NAME = 'MARTY'
+# User who is running the script
+USERNAME = os.getlogin()
+# Default install path, can customise to run multiple instances
+KMD_CONF_PATH = os.getenv(f'KMD_CONF_PATH')
+# Address to be used for daemon
+KMD_ADDRESS = os.getenv(f'KMD_ADDRESS')
+# Pubkey to be used for daemon
+KMD_PUBKEY = os.getenv(f'KMD_PUBKEY')
+# User home path
+HOME_PATH = os.getenv(f'HOME_PATH')
+# User home path
+LOCALHOST_ONLY = os.getenv(f'LOCALHOST_ONLY') in ['true', True]
+if not HOME_PATH:
+    HOME_PATH = f'/home/{USERNAME}/'
+if not KMD_CONF_PATH:
+    KMD_CONF_PATH = f'/home/{USERNAME}/.komodo/'
+if not KMD_ADDRESS:
+    # S7 Dragonhound_DEV main notary address
+    KMD_ADDRESS = "RHi882Amab35uXjqBZjVxgEgmkkMu454KK"
+if not KMD_PUBKEY:
+    # S7 Dragonhound_DEV main notary pubkey
+    KMD_PUBKEY = "02f9a7b49282885cd03969f1f5478287497bc8edfceee9eac676053c107c5fcdaf"
+if not LOCALHOST_ONLY:
+    LOCALHOST_ONLY = False
