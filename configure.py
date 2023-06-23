ffac2ebbce3bf47300533728faa1e764ccd6601b
smk762 <smk762@iinet.net.au>
2023-06-23 11:35:18 +0800

    paths

--- configure.py
+++ configure.py
@@ -9,18 +9,21 @@ import string
 import secrets
 import requests
 import const
+from pathlib import Path
 from dotenv import load_dotenv
 from logger import logger
 
 load_dotenv()
 
+
 class Utils:
     def __init__(self):
         self.script_path = os.path.realpath(os.path.dirname(__file__))
+        self.project_root = Path(self.script_path).parent
 
     def get_random_string(self, length=16):
         char_pool = string.ascii_uppercase + string.ascii_lowercase + string.digits
-        return ''.join(secrets.choice(char_pool) for i in range(length))
+        return "".join(secrets.choice(char_pool) for i in range(length))
 
     def get_my_ip(self):
         return requests.get("https://ifconfig.me").text
@@ -35,7 +38,6 @@ class Utils:
                     logger.info(f"Launch params for {coin} not found.")
                     return None
             return launch_params[coin]
-        
 
     def update_launch_params(self, coin):
         launch_params = self.get_launch_params(coin)
@@ -55,27 +57,30 @@ class Utils:
             return {}
 
     def get_explorers(self):
-        with open(f"{self.script_path}/../coin_ports.json", "r") as f:
+        print(f"{self.project_root}/coin_ports.json")
+        with open(f"{self.project_root}/coin_ports.json", "r") as f:
             coin_ports = json.load(f)
         try:
             with open(f"{self.script_path}/explorers.json", "r") as f:
                 explorers = json.load(f)
                 for coin in explorers:
                     if coin in coin_ports:
-                        explorers[coin].update({
-                            "rpcport": coin_ports[coin]["rpcport"],
-                            "zmqport": coin_ports[coin]["zmqport"],
-                            "webport": coin_ports[coin]["webport"]
-                        })
+                        explorers[coin].update(
+                            {
+                                "rpcport": coin_ports[coin]["rpcport"],
+                                "zmqport": coin_ports[coin]["zmqport"],
+                                "webport": coin_ports[coin]["webport"],
+                            }
+                        )
                 return explorers
         except:
             return {}
 
-    def get_coin_conf(self, coin):        
+    def get_coin_conf(self, coin):
         coin_conf_file = self.get_coin_conf_file(coin)
         conf_data = {}
         if os.path.exists(coin_conf_file):
-            with open(coin_conf_file, 'r') as f:
+            with open(coin_conf_file, "r") as f:
                 lines = f.readlines()
                 for line in lines:
                     k, v = line.split("=")
@@ -103,21 +108,24 @@ class ConfigExplorer:
         self.explorers = self.utils.get_explorers()
 
     def create_launcher(self):
-        with open(f"{self.coin}-explorer-start.sh", 'w') as f:
-            f.write('#!/bin/bash\n')
+        with open(f"{self.coin}-explorer-start.sh", "w") as f:
+            f.write("#!/bin/bash\n")
             f.write(f'export NVM_DIR="{self.home}/.nvm"\n')
-            f.write(f'[ -s "{self.home}/.nvm/nvm.sh" ] && . "{self.home}/.nvm/nvm.sh" # This loads nvm\n')
-            f.write(f'cd {self.coin}-explorer\n')
-            f.write('nvm use v4; ./node_modules/bitcore-node-komodo/bin/bitcore-node start\n')
+            f.write(
+                f'[ -s "{self.home}/.nvm/nvm.sh" ] && . "{self.home}/.nvm/nvm.sh" # This loads nvm\n'
+            )
+            f.write(f"cd {self.coin}-explorer\n")
+            f.write(
+                "nvm use v4; ./node_modules/bitcore-node-komodo/bin/bitcore-node start\n"
+            )
         os.chmod(f"{self.coin}-explorer-start.sh", stat.S_IRWXU)
 
     def create_explorer_conf(self):
-
         conf_data = self.utils.get_coin_conf(self.coin)
         rpcuser = conf_data["rpcuser"]
         rpcpassword = conf_data["rpcpassword"]
-            
-        with open(f"{self.script_path}/../coin_ports.json", "w+") as f:
+
+        with open(f"{self.project_root}/coin_ports.json", "w+") as f:
             coin_ports = json.load(f)
             rpcport = coin_ports["rpcport"]
             zmqport = coin_ports["zmqport"]
@@ -127,19 +135,23 @@ class ConfigExplorer:
             self.explorers.update({self.coin: {}})
         rpcip = "127.0.0.1"
         with open(f"{self.script_path}/explorers.json", "w+") as f:
-            self.explorers[self.coin].update({
-                "rpcip": rpcip,
-                "rpcport": rpcport,
-                "rpcuser": rpcuser,
-                "rpcpassword": rpcpassword,
-                "webport": webport,
-                "zmqport": zmqport
-            })
+            self.explorers[self.coin].update(
+                {
+                    "rpcip": rpcip,
+                    "rpcport": rpcport,
+                    "rpcuser": rpcuser,
+                    "rpcpassword": rpcpassword,
+                    "webport": webport,
+                    "zmqport": zmqport,
+                }
+            )
             json.dump(self.explorers, f, indent=4)
             explorer_index = len(self.explorers)
             logger.info(f"{explorer_index} explorers configured")
 
-        with open(f"{self.script_path}/{self.coin}-explorer/bitcore-node.json", "w+") as f:
+        with open(
+            f"{self.script_path}/{self.coin}-explorer/bitcore-node.json", "w+"
+        ) as f:
             config = {
                 "network": "mainnet",
                 "port": webport,
@@ -147,52 +159,59 @@ class ConfigExplorer:
                     "bitcoind",
                     "insight-api-komodo",
                     "insight-ui-komodo",
-                    "web"
+                    "web",
                 ],
                 "servicesConfig": {
                     "bitcoind": {
-                        "connect": [{
-                            "rpchost": rpcip,
-                            "rpcport": rpcport,
-                            "rpcuser": rpcuser,
-                            "rpcpassword": rpcpassword,
-                            "zmqpubrawtx": f"tcp://{rpcip}:{zmqport}"
-                        }]
+                        "connect": [
+                            {
+                                "rpchost": rpcip,
+                                "rpcport": rpcport,
+                                "rpcuser": rpcuser,
+                                "rpcpassword": rpcpassword,
+                                "zmqpubrawtx": f"tcp://{rpcip}:{zmqport}",
+                            }
+                        ]
                     },
                     "insight-api-komodo": {
                         "rateLimiterOptions": {
-                            "whitelist": [f"::ffff:{rpcip}",rpcip],
+                            "whitelist": [f"::ffff:{rpcip}", rpcip],
                             "whitelistLimit": 500000,
-                            "whitelistInterval": 3600000
+                            "whitelistInterval": 3600000,
                         }
-                    }
-                }
+                    },
+                },
             }
             json.dump(config, f, indent=4)
 
     def create_webaccess(self, noweb=False):
-
         conf_data = self.utils.get_coin_conf(self.coin)
         rpcuser = conf_data["rpcuser"]
         rpcpassword = conf_data["rpcpassword"]
-            
-        with open(f"{self.script_path}/../coin_ports.json", "w+") as f:
+
+        with open(f"{self.project_root}/coin_ports.json", "w+") as f:
             coin_ports = json.load(f)
             rpcport = coin_ports["rpcport"]
             zmqport = coin_ports["zmqport"]
             webport = coin_ports["webport"]
-            
+
         rpcip = "127.0.0.1"
 
         if noweb:
-            ip = 'localhost'
-            logger.info(f"The webport hasn't been opened. To access the explorer through the internet,")
-            logger.info(f"open the port: {webport} by executing the command 'sudo ufw allow {webport}'")
+            ip = "localhost"
+            logger.info(
+                f"The webport hasn't been opened. To access the explorer through the internet,"
+            )
+            logger.info(
+                f"open the port: {webport} by executing the command 'sudo ufw allow {webport}'"
+            )
         else:
             ip = self.utils.get_my_ip()
 
-        logger.info(f"Visit http://{ip}:{webport} from another computer to access the explorer after starting it")
-        with open(f'{self.coin}-webaccess', 'w') as f:
+        logger.info(
+            f"Visit http://{ip}:{webport} from another computer to access the explorer after starting it"
+        )
+        with open(f"{self.coin}-webaccess", "w") as f:
             f.write(f"url=http://{ip}:{webport}\n")
             f.write(f"webport={webport}")
 
@@ -201,19 +220,21 @@ class ConfigExplorer:
             if self.coin in self.explorers:
                 del self.explorers[self.coin]
                 json.dump(self.explorers, f, indent=4)
-                logger.info(f"{self.coin} removed from {self.script_path}/explorers.json")
-        for file in [f'{self.coin}-webaccess', f'{self.coin}-explorer-start.sh']:
+                logger.info(
+                    f"{self.coin} removed from {self.script_path}/explorers.json"
+                )
+        for file in [f"{self.coin}-webaccess", f"{self.coin}-explorer-start.sh"]:
             try:
                 os.remove(file)
             except OSError as e:
                 logger.info(f"{file} does not exist, skipping...")
                 pass
         try:
-            os.remove(f'{self.coin}-webaccess')
+            os.remove(f"{self.coin}-webaccess")
         except OSError as e:
             logger.info(f"{f'{self.coin}-webaccess'} does not exist, skipping...")
         try:
-            shutil.rmtree(f'{self.coin}-explorer')
+            shutil.rmtree(f"{self.coin}-explorer")
         except OSError as e:
             logger.info(f"{self.coin}-explorer does not exist, skipping...")
 
@@ -230,21 +251,21 @@ class ConfigNginx:
 
     def setup(self):
         home = os.path.expanduser("~")
-        
+
         conf_data = self.utils.get_coin_conf(self.coin)
         rpcuser = conf_data["rpcuser"]
         rpcpassword = conf_data["rpcpassword"]
-            
-        with open(f"{self.script_path}/../coin_ports.json", "w+") as f:
+
+        with open(f"{self.project_root}/coin_ports.json", "w+") as f:
             coin_ports = json.load(f)
             rpcport = coin_ports["rpcport"]
             zmqport = coin_ports["zmqport"]
             webport = coin_ports["webport"]
-            
+
         rpcip = "127.0.0.1"
         webroot = f"{home}/insight"
         os.makedirs(webroot, exist_ok=True)
-        proxy_host = '127.0.0.1'
+        proxy_host = "127.0.0.1"
 
         self.update_webaccess(webport)
         explorer = ConfigExplorer(self.coin)
@@ -252,12 +273,12 @@ class ConfigNginx:
         self.create_serverblock(webroot, proxy_host, webport)
 
     def update_webaccess(self, port):
-        with open(f'{self.coin}-webaccess', 'w') as f:
+        with open(f"{self.coin}-webaccess", "w") as f:
             f.write(f"url=http://{self.subdomain}:{port}\n")
             f.write(f"webport={port}")
 
     def check_webaccess(self):
-        if not os.path.exists(f'{self.script_path}/{self.coin}-webaccess'):
+        if not os.path.exists(f"{self.script_path}/{self.coin}-webaccess"):
             return None
         with open(f"{self.script_path}/{self.coin}-webaccess", "r") as r:
             for line in r.readlines():
@@ -284,7 +305,9 @@ class ConfigServices:
         self.address = address
         self.pubkey = pubkey
         self.utils = Utils()
-        self.launch_params = f"{self.utils.get_launch_params(self.coin)} -pubkey=${self.pubkey}"
+        self.launch_params = (
+            f"{self.utils.get_launch_params(self.coin)} -pubkey=${self.pubkey}"
+        )
         self.script_path = os.path.realpath(os.path.dirname(__file__))
         self.explorers = self.utils.get_explorers()
         if not os.path.exists(f"{const.HOME_PATH}/logs"):
@@ -293,7 +316,9 @@ class ConfigServices:
     def create_daemon_service(self):
         with open(f"{self.script_path}/services/TEMPLATE-daemon.service", "r") as f:
             lines = f.readlines()
-            with open(f"{self.script_path}/services/{self.coin}-daemon.service", "w") as f:
+            with open(
+                f"{self.script_path}/services/{self.coin}-daemon.service", "w"
+            ) as f:
                 for line in lines:
                     line = line.replace("COIN", self.coin)
                     line = line.replace("ADDRESS", const.KMD_ADDRESS)
@@ -304,18 +329,25 @@ class ConfigServices:
     def create_explorer_service(self):
         with open(f"{self.script_path}/services/TEMPLATE-explorer.service", "r") as f:
             lines = f.readlines()
-            with open(f"{self.script_path}/services/{self.coin}-explorer.service", "w") as f:
+            with open(
+                f"{self.script_path}/services/{self.coin}-explorer.service", "w"
+            ) as f:
                 for line in lines:
                     line = line.replace("COIN", self.coin)
                     line = line.replace("USERNAME", const.USERNAME)
                     f.write(line)
 
+
 def main():
     # TODO: Update .env file?
     options = [
-        "create_nginx_conf", "create_services",
-        "create_explorer_conf", "create_webaccess", "get_launch_params",
-        "remove", "exit"        
+        "create_nginx_conf",
+        "create_services",
+        "create_explorer_conf",
+        "create_webaccess",
+        "get_launch_params",
+        "remove",
+        "exit",
     ]
     option = None
     if len(sys.argv) > 1:
@@ -332,7 +364,9 @@ def main():
                 i = int(input("Select an option: ")) - 1
                 option = options[i]
             except:
-                logger.warning(f"Invalid option - must be an integer between 1 and {len(options)}")
+                logger.warning(
+                    f"Invalid option - must be an integer between 1 and {len(options)}"
+                )
 
     if len(sys.argv) > 2:
         ticker = sys.argv[2]
@@ -345,12 +379,14 @@ def main():
         if len(sys.argv) > 3:
             subdomain = sys.argv[3]
         else:
-            subdomain = os.getenv(f'{ticker}_SUBDOMAIN')
+            subdomain = os.getenv(f"{ticker}_SUBDOMAIN")
         if not subdomain:
-            subdomain = input(f"Enter the {ticker} explorer's subdomain name (e.g. kmd.explorer.io): ")
+            subdomain = input(
+                f"Enter the {ticker} explorer's subdomain name (e.g. kmd.explorer.io): "
+            )
         nginx = ConfigNginx(ticker, subdomain)
         nginx.setup()
-    
+
     elif option == "get_launch_params":
         utils = Utils()
         print(f"komodod {utils.get_launch_params(ticker)} -pubkey=${const.KMD_PUBKEY}")
@@ -384,5 +420,5 @@ def main():
             sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
