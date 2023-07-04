4d0ed2fe05ee67765de480ff7ed1a26279057e80
smk762 <smk762@iinet.net.au>
2023-07-05 02:34:38 +0800

    only update bitcore conf

--- configure-docker.py
+++ configure-docker.py
@@ -109,8 +109,7 @@ def main():
     script_path = os.path.dirname(os.path.abspath(__file__))
 
     config = ConfigExplorer(ticker)
-    config.create_explorer_conf()
-    config.create_launcher()
+    config.save_bitcore_conf()
 
 if __name__ == "__main__":
     main()
